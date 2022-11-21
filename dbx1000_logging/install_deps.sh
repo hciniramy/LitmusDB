3312221f32e1841a3e72fc8f833e3acdbf687bbe
Ramy Hcini <hssini.ramy@gmail.com>
2022-11-21 21:44:31 +0100

    force-install all deps with apt

--- dbx1000_logging/install_deps.sh
+++ dbx1000_logging/install_deps.sh
@@ -1,15 +1,15 @@
 apt-get update
-apt-get install -y --no-install-recommends apt-utils
-apt-get install -y --no-install-recommends python3
-apt-get install -y --no-install-recommends wget
-apt-get install -y --no-install-recommends numactl
+apt-get install -y --force-yes --no-install-recommends apt-utils
+apt-get install -y --force-yes --no-install-recommends python3
+apt-get install -y --force-yes --no-install-recommends wget
+apt-get install -y --force-yes --no-install-recommends numactl
 #apt-get install -y openjdk-14-jdk
-apt-get install -y --no-install-recommends libjemalloc-dev
-apt-get install -y --no-install-recommends libntl-dev
+apt-get install -y --force-yes --no-install-recommends libjemalloc-dev
+apt-get install -y --force-yes --no-install-recommends libntl-dev
 # on some systems we need to install add-apt-repository 
-apt-get install -y --no-install-recommends apt-transport-https software-properties-common 
+apt-get install -y --force-yes --no-install-recommends apt-transport-https software-properties-common
 apt-get update
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
 add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-apt-get update && apt-get install -y --no-install-recommends adoptopenjdk-14-hotspot
+apt-get update && apt-get install -y --force-yes --no-install-recommends adoptopenjdk-14-hotspot
 
