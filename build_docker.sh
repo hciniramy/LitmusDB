9672df858853a3103c414ce74b169dea22097acf
Ramy Hcini <hssini.ramy@gmail.com>
2022-11-26 16:51:34 +0100

    fix long attribute format

--- build_docker.sh
+++ build_docker.sh
@@ -14,12 +14,12 @@ fi
 
 echo "[Script-INFO] Building litmus docker image"
 docker build -t $8  \
-  -build-arg BUILD_REF=$1 \
-  -build-arg BUILD_ALG=$2 \
-  -build-arg BUILD_CC_ALG=$3 \
-  -build-arg BUILD_WORKLOAD=$4 \
-  -build-arg BUILD_MEM_INTEGRITY=$5 \
-  -build-arg BUILD_VERIFICATION=$6 \
-  -build-arg BUILD_ELLE_OUTPUT=$7 \
+  --build-arg BUILD_REF=$1 \
+  --build-arg BUILD_ALG=$2 \
+  --build-arg BUILD_CC_ALG=$3 \
+  --build-arg BUILD_WORKLOAD=$4 \
+  --build-arg BUILD_MEM_INTEGRITY=$5 \
+  --build-arg BUILD_VERIFICATION=$6 \
+  --build-arg BUILD_ELLE_OUTPUT=$7 \
   -f docker/Dockerfile .
 
