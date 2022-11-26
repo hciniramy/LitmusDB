06c4d465facb9dd5aacf9e9f3eba6dac13305493
Ramy Hcini <hssini.ramy@gmail.com>
2022-11-26 15:45:52 +0100

    fix arguments passed to litmusdb docker build

--- build_docker.sh
+++ build_docker.sh
@@ -13,12 +13,13 @@ else
 fi
 
 echo "[Script-INFO] Building litmus docker image"
-docker build -t $7  \
+docker build -t $8  \
   -build-arg BUILD_REF=$1 \
   -build-arg BUILD_ALG=$2 \
   -build-arg BUILD_CC_ALG=$3 \
-  -build-arg BUILD_MEM_INTEGRITY=$4 \
-  -build-arg BUILD_VERIFICATION=$5 \
-  -build-arg BUILD_ELLE_OUTPUT=$6 \
+  -build-arg BUILD_WORKLOAD=$4 \
+  -build-arg BUILD_MEM_INTEGRITY=$5 \
+  -build-arg BUILD_VERIFICATION=$6 \
+  -build-arg BUILD_ELLE_OUTPUT=$7 \
   -f docker/Dockerfile .
 
